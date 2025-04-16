bc2b47bbfe102f236f3da554309b640270cf4a18
Kasidit Chaipech <benkasidit2945@gmail.com>
2025-04-16 16:29:41 +0700

    Fix Bug on User Page and create Repair Record

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -71,7 +71,19 @@ export const UserController = {
                 select: {
                     id: true,
                     username: true,
-                    level: true
+                    level: true,
+                    section: {
+                        select:{
+                            name: true,
+                            id: true,
+                            department: {
+                                select: {
+                                    name: true,
+                                    id: true
+                                }
+                            }
+                        }
+                    }
                 },
                 where: {
                     status: "active"
@@ -91,7 +103,7 @@ export const UserController = {
             username: string;
             password: string;
             level: string;
-            sectionId: string;
+            sectionId: number;
         }
     }) => {
         try {
@@ -109,7 +121,7 @@ export const UserController = {
             username: string;
             password: string;
             level: string;
-            sectionId: string;
+            sectionId: number;  // must be the same type from front. 
         },
         params: {
             id: string;
@@ -120,19 +132,18 @@ export const UserController = {
                 select: { password: true },
                 where: { id: parseInt(params.id) }
             })
-            if (body.password.trim().length > 0) {
-                const newData = {
-                    username: body.username,
-                    password: body.password == '' ? oldUser?.password : body.password,
-                    level: body.level,
-                    sectionId: body.sectionId
-                }
-                
-                await prisma.user.update({
-                    where: { id: parseInt(params.id)},
-                    data: newData
-                })
+
+            const newData = {
+                username: body.username,
+                password: body.password == '' ? oldUser?.password : body.password,
+                level: body.level,
+                sectionId: body.sectionId
             }
+                
+            await prisma.user.update({
+                where: { id: parseInt(params.id)},
+                data: newData
+            })
         } catch (error) {
             return error;
         }
